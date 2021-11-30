dafcffe9faf7e093dc342c3984d9a993b8321ba8
gwansik <rhkstlr00@gmail.com>
2021-11-30 18:05:58 +0900

    while문으로 구구단 만들어보기

--- javascript/js8.js
+++ javascript/js8.js
@@ -13,3 +13,39 @@ while (i < 101) {
     result += 1;
     i += 2;
 }
+
+// do
+let num = 0;
+
+do {
+    console.log(num);
+    num += 1;
+} while (num < 11);
+
+//while문 구구단
+
+let i = 2;
+let j = 1;
+while (i < 10) {
+    while (j < 10) {
+        console.log(`${i} X ${j} = ${i * j}`);
+        j++;
+    }
+    i++;
+    j = 1;
+}
+
+// while문 구구단 5단에서 멈추게 하는 방법
+let i = 2;
+let j = 1;
+while (i < 10) {
+    while (j < 10) {
+        console.log(`${i} X ${j} = ${i * j}`);
+        if (j == 5) {
+            break;
+        }
+        j++;
+    }
+    i++;
+    j = 1;
+}
