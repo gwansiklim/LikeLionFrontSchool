d26a6010a4155abee8605d1f5bb84ddb7f579cf0
gwansik <rhkstlr00@gmail.com>
2021-12-27 13:57:01 +0900

    postman 연결하여 데이터 요청하기

--- node/first_project/routes/index.js
+++ node/first_project/routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+let arr = [];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/read', function(req, res) {
+  res.status(200).json ({
+    message: "read success",
+  });
 });
+
+/* POST */
+router.post("/create", (req, res) => {
+  const {data} = req.body;
+  arr.push(data);
+  res.status(200).json({
+    message: "create success",
+    result: arr,
+  })
+})
+
+/* PUT */
+router.put("/update/:id", (req, res) => {
+  const {id} = req.params;
+  const {data} =req.body;
+  arr[id] = data;
+  res.status(200).json({
+    message: "update success",
+    result: arr,
+  })
+})
+
+/* DELETE */
+router.delete("/delete/:id", (req, res) => {
+  const {id} = req.params;
+  arr.splice(id, 1);
+  res.status(200).json({
+    message: "delete success",
+    result: arr
+  })
+})
 
 module.exports = router;
