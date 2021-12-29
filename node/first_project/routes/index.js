08e2f1bfa02d449b6b1c7ce1b2b8e22be4aa9f80
gwansik <rhkstlr00@gmail.com>
2021-12-29 17:30:46 +0900

    crud 생성하는 방법

--- node/first_project/routes/index.js
+++ node/first_project/routes/index.js
@@ -23,4 +23,69 @@ router.post('/', async (req, res) => {
 
 });
 
+router.get('/', async (req, res) => {
+  try {
+    const result = await postModel.find({});
+    res.status(200).json({
+      message: "read success!!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const {id} = req.params;
+  try{
+    const result = await postModel.findById(id);
+    res.status(200).json({
+      message: "detail success!!",
+      data: result,
+    });
+  }catch(error){
+    res.status(500).json({
+      message: error,
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const {id} = req.params;
+  const {title, content} = req.body;
+  
+  try {
+    const result = await postModel.findByIdAndUpdate(id, {
+      title: title,
+      content: content,
+    }, {
+      new: true,
+  });
+    res.status(200).json({
+      message: "update success!!",
+      data: result,
+  })
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+  })
+}
+})
+
+router.delete('/:id', async (req, res) => {
+  const {id} = req.params;
+  try{
+    await postModel.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "delete success!!",    
+    })
+  }catch(error){
+    res.status(500).json({
+      message: error,
+    });
+  }
+})
+
 module.exports = router;
